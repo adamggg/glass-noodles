566c4c257cef19c66f3649a4df03ac2bee1e2ad0
abdelrahmansakr <abdelrahmansakr@gmail.com>
2014-11-15 21:44:38 +0200

    Adding instance variable 'config file'

--- src/cache.java
+++ src/cache.java
@@ -1,21 +1,24 @@
+import java.io.File;
 import java.util.HashMap;
 
 
 public class cache {
 	
 	HashMap<Integer, int[]> cache; 
+	File configuration_file;
 	int s;
 	int l;
 	int m;
-	int index_bits;
-	int offset_bits;
-	int tag_bits;
 	String write_policy;
 	int number_of_cycles;
 	int memory_access_time;
+	int tag_bits;
+	int index_bits;
+	int offset_bits;
+
 	
-	public cache(int s, int l, int m){
-		
+	public cache(File configuration_file){
+		this.configuration_file = configuration_file;	
 		
 	}
 
