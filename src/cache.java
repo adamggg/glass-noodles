fc9d692001ffc37bdce74d0f5c100bfb994dc715
abdelrahmansakr <abdelrahmansakr@gmail.com>
2014-11-15 21:13:31 +0200

    Adding instance variables

--- src/cache.java
+++ src/cache.java
@@ -1,4 +1,18 @@
 
 public class cache {
+	
+	int s;
+	int l;
+	int m;
+	int index_bits;
+	int offset_bits;
+	int tag_bits;
+	String write_policy;
+	int number_of_cycles;
+	int memory_access_time;
+	
+	public cache(int s, int l, int m){
+		
+	}
 
 }
