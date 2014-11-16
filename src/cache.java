76e253b6f663492e2af0fea8c1ec105e3c87995b
abdelrahmansakr <abdelrahmansakr@gmail.com>
2014-11-16 19:12:07 +0200

    Adding readCache method and fixing code conventions

--- src/cache.java
+++ src/cache.java
@@ -9,55 +9,67 @@ import java.util.HashMap;
 public class cache {
 	
 	HashMap<Integer, String[][]> cache; 
-	File configuration_file;
+	File configurationFile;
 	int s;
 	int l;
 	int m;
-	String write_policy;
-	int number_of_cycles;
-	int memory_access_time;
-	int tag_bits;
-	int index_bits;
-	int offset_bits;
-	int base_address;
-	int number_of_lines;
-	int number_of_sets;
+	String writePolicy;
+	int numberOfCycles;
+	int memoryAccessTime;
+	int tagBits;
+	int indexBits;
+	int offsetBits;
+	int baseAddress;
+	int numberOfLines;
+	int numberOfSets;
 	
-	public cache(File configuration_file) throws NumberFormatException, IOException{
-		this.configuration_file = configuration_file;	
-		BufferedReader configuration_file_reader = new BufferedReader(new FileReader(this.configuration_file)); 
-		s = Integer.parseInt(configuration_file_reader.readLine());
-		l = Integer.parseInt(configuration_file_reader.readLine());
-		m = Integer.parseInt(configuration_file_reader.readLine());
-		write_policy = configuration_file_reader.readLine();
-		number_of_cycles = Integer.parseInt(configuration_file_reader.readLine());
-		memory_access_time = Integer.parseInt(configuration_file_reader.readLine()); // Heya di tefre2 eh 3an elly ablaha ?!
-		configuration_file_reader.close();
+	public cache(File configurationFile) throws NumberFormatException, IOException{
+		this.configurationFile = configurationFile;	
+		BufferedReader configurationFileReader = new BufferedReader(new FileReader(this.configurationFile)); 
+		s = Integer.parseInt(configurationFileReader.readLine());
+		l = Integer.parseInt(configurationFileReader.readLine());
+		m = Integer.parseInt(configurationFileReader.readLine());
+		writePolicy = configurationFileReader.readLine();
+		numberOfCycles = Integer.parseInt(configurationFileReader.readLine());
+		memoryAccessTime = Integer.parseInt(configurationFileReader.readLine()); // Heya di tefre2 eh 3an elly ablaha ?!
+		configurationFileReader.close();
 		
-		number_of_lines = (s * 1024) / l;
-		offset_bits = (int) (Math.log(l) / Math.log(2));
-		index_bits = (int) (Math.log(number_of_lines) / Math.log(2));
-		tag_bits = 16 - (offset_bits + index_bits);
-		number_of_sets = number_of_lines / m;
+		numberOfLines = (s * 1024) / l;
+		offsetBits = (int) (Math.log(l) / Math.log(2));
+		indexBits = (int) (Math.log(numberOfLines) / Math.log(2));
+		tagBits = 16 - (offsetBits + indexBits);
+		numberOfSets = numberOfLines / m;
 		
-		for(int i = 0; i < number_of_sets ; i++){
+		for(int i = 0; i < numberOfSets ; i++){
 			cache.put(i, new String[m][4]);
 		}
 	}
 	
-	public String read_cache(String address){
-		return "";
+	public String readCache(String address){
 		
+		int index = Integer.parseInt(splitAddress(address).get("index"), 2);		
+		String[][] cacheSet = cache.get(index);
+		
+		for(int i = 0 ; i < cacheSet.length ; i++){
+				if (cacheSet[i][2].equalsIgnoreCase(splitAddress(address).get("tag"))) {
+					return cacheSet[i][3];
+			}
+		}
+		
+		String data = readMemory(address);
+		writeCache(address, data);
+		
+		return data;
 	}
 	
-	public HashMap<String, String> split_address(String address){
-		HashMap<String, String> splitted_address = new HashMap<String, String>();
+	public HashMap<String, String> splitAddress(String address){
+		HashMap<String, String> splittedAddress = new HashMap<String, String>();
 
-		splitted_address.put("tag", address.substring(0, tag_bits));
-		splitted_address.put("index", address.substring(tag_bits, tag_bits+index_bits));
-		splitted_address.put("offset" , address.substring(tag_bits+index_bits, tag_bits+index_bits+offset_bits));
+		splittedAddress.put("tag", address.substring(0, tagBits));
+		splittedAddress.put("index", address.substring(tagBits, tagBits + indexBits));
+		splittedAddress.put("offset" , address.substring(tagBits + indexBits, tagBits + indexBits + offsetBits));
 		
-		return splitted_address;		
+		return splittedAddress;		
 	}
 
 }
