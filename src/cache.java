697da394ce4f02115c87546a826890891eb5d628
mahmoudawadeen <mahmoud.awadeen@gmail.com>
2014-11-17 01:07:50 +0200

    return in write cache

--- src/cache.java
+++ src/cache.java
@@ -9,7 +9,7 @@ import com.sun.corba.se.impl.ior.WireObjectKeyTemplate;
 
 
 public class cache {
-	
+
 	HashMap<Integer, String[][]> cache; 
 	File configurationFile;
 	int s;
@@ -24,7 +24,7 @@ public class cache {
 	int baseAddress;
 	int numberOfLines;
 	int numberOfSets;
-	
+
 	public cache(File configurationFile) throws NumberFormatException, IOException{
 		this.configurationFile = configurationFile;	
 		BufferedReader configurationFileReader = new BufferedReader(new FileReader(this.configurationFile)); 
@@ -35,42 +35,42 @@ public class cache {
 		numberOfCycles = Integer.parseInt(configurationFileReader.readLine());
 		memoryAccessTime = Integer.parseInt(configurationFileReader.readLine()); // Heya di tefre2 eh 3an elly ablaha ?!
 		configurationFileReader.close();
-		
+
 		numberOfLines = (s * 1024) / l;
 		offsetBits = (int) (Math.log(l) / Math.log(2));
 		indexBits = (int) (Math.log(numberOfLines) / Math.log(2));
 		tagBits = 16 - (offsetBits + indexBits);
 		numberOfSets = numberOfLines / m;
-		
+
 		for(int i = 0; i < numberOfSets ; i++){
 			cache.put(i, new String[m][4]);
 		}
 	}
-	
+
 	public String readCache(String address){
-		
+
 		int index = Integer.parseInt(splitAddress(address).get("index"), 2);		
 		String[][] cacheSet = cache.get(index);
-		
+
 		for(int i = 0 ; i < cacheSet.length ; i++){
-				if (cacheSet[i][2].equalsIgnoreCase(splitAddress(address).get("tag"))) {
-					return cacheSet[i][3];
+			if (cacheSet[i][2].equalsIgnoreCase(splitAddress(address).get("tag"))) {
+				return cacheSet[i][3];
 			}
 		}
-		
+
 		String data = readMemory(address);
 		writeCache(address, data);
-		
+
 		return data;
 	}
-	
+
 	public HashMap<String, String> splitAddress(String address){
 		HashMap<String, String> splittedAddress = new HashMap<String, String>();
 
 		splittedAddress.put("tag", address.substring(0, tagBits));
 		splittedAddress.put("index", address.substring(tagBits, tagBits + indexBits));
 		splittedAddress.put("offset" , address.substring(tagBits + indexBits, tagBits + indexBits + offsetBits));
-		
+
 		return splittedAddress;		
 	}
 	public boolean writeCache(String address , String data){
@@ -81,11 +81,14 @@ public class cache {
 			cacheSet[cacheSet.length][1]="0";
 			cacheSet[cacheSet.length][2]=splittedAddress.get("tag");
 			cacheSet[cacheSet.length][3]=data;
-			}else
-				replace(cacheSet,address,data);
-		return false;
+			
+		}else{
+			replace(cacheSet,address,data);
+			
+		}
+		return cacheSet.length-1 < m;
 	}
-	
+
 	public boolean replace(String [][] cacheSet,String address,String data){
 		boolean dirty=false;
 		int chosenToReplaceWith = (int) Math.random()%m;
