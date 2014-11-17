ac8082d9571008bc85caec2954eb72bd6062b30f
mahmoudawadeen <mahmoud.awadeen@gmail.com>
2014-11-17 18:31:20 +0200

    added Micro class with instance variables

---
+++ src/Microprocessor.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+
+public class Microprocessor {
+
+	ArrayList<cache> cacheLevels;
+	memory memory;
+	int pc;
+	int numberOfInstructionsExcuted;
+	int totalNumberOfCyclesSpentForMemory;
+}
