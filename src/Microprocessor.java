e58dafa3a54dacb08975172419b232c8d44f0248
narimanhesham <nariman.hesham93@gmail.com>
2014-11-23 22:43:15 +0200

    adding ADDI instruction

--- src/Microprocessor.java
+++ src/Microprocessor.java
@@ -125,6 +125,15 @@ public class Microprocessor {
 		}
 		else if(data.startsWith("111")) {
 			//AddI
+			int regA = Integer.parseInt(data.substring(3, 6));
+			int regB = Integer.parseInt(data.substring(6, 9));
+			int immediateValue = signedBinaryToDecimal(data.substring(9, 16));
+			
+			int result = Integer.parseInt(registers.get(regB), 2) + immediateValue;
+			
+			if (regA != 0) {
+				registers.put(regA, to16BinaryStringValue(result));
+			}
 		}
 		else if(data.startsWith("0000000")) {
 			//Add
